492f31ca52f991380f48dbdb1e7dbf7391f80de4
phillipaimusic <phillip@aimusic.co.uk>
2018-09-08 17:27:46 +0100

    husky test

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -16,7 +16,6 @@ interface ContainerProps {
   className?: string
 }
 
-
 const Container: React.SFC<ContainerProps> = ({ children, className }) => (
   <StyledContainer className={className}>{children}</StyledContainer>
 )

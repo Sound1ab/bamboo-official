bbd6ce105b052cfcdf31a2f538e8d9f3f0fb3a0a
phillipaimusic <phillip@aimusic.co.uk>
2018-09-17 19:59:31 +0100

    Moved storybook wrapper component into seperate file

---
+++ .storybook/Wrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'react-emotion'
+
+export const Wrapper = styled('div')`
+  position: absolute;
+  padding: 8px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
--- .storybook/config.js
+++ .storybook/config.js
@@ -2,25 +2,13 @@ import React from 'react'
 import 'modern-normalize'
 import { ThemeProvider } from '../src/components/atoms/ThemeProvider'
 import { configure, addDecorator } from '@storybook/react'
-import styled from 'react-emotion'
+import { Wrapper } from './Wrapper'
 import '../src/theme/normalize'
 
-const Wrapper = styled('div')`
-  position: absolute;
-  padding: 8px;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-`
-
-addDecorator(story => (
-  <Wrapper>
-    <ThemeProvider>{story()}</ThemeProvider>
-  </Wrapper>
-))
+addDecorator(story => {
+  const Theme = React.createElement(ThemeProvider, {}, story())
+  return React.createElement(Wrapper, {}, Theme)
+})
 
 const req = require.context('../src', true, /.stories.tsx$/)
 function loadStories() {
